f87bb9a20d0551652b0e008ff46124fd155a3010
larrysharpe <larry.l.sharpe@gmail.com>
2018-02-17 09:15:30 -0500

    added global header and global footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <router-view/>
+    <header>
+      <header-global></header-global>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      <footer-global></footer-global>
+    </footer>
   </div>
 </template>
 
 <script>
+import FooterGlobal from '@/components/FooterGlobal.vue'
+import HeaderGlobal from '@/components/HeaderGlobal.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { FooterGlobal, HeaderGlobal }
 }
 </script>
---
+++ src/components/FooterGlobal.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="text-muted">Place footer content here.</span>
+  </div>
+</template>
